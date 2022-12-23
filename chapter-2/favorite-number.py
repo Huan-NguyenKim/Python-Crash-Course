bedbe66ed7d1f740cb2561fe33a04652e3301f88
Huan Nguyen <ngkimhuan@hotmail.com>
2022-12-23 03:59:10 -0800

    updating favorite-number.py

--- chapter-2/favorite-number.py
+++ chapter-2/favorite-number.py
@@ -1,3 +1,6 @@
 #2-9. Favorite Number: Use a variable to represent your favorite number. Then,
 # using that variable, create a message that reveals your favorite number. 
 # Print that message.
+
+favorite_number = 8
+print(f"Huan's favorite number is", favorite_number)
