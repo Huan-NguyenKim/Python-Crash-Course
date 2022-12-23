64d67fab87b666cde22ace17fc66bd5d60105cf7
Huan Nguyen <ngkimhuan@hotmail.com>
2022-12-23 02:43:31 -0800

    adding personal-message.py to chapter2

---
+++ chapter-2/personal-message.py
@@ -0,0 +1,6 @@
+#2-3. Personal Message: Use a variable to represent a person’s name, and print
+#a message to that person. Your message should be simple, such as, 
+# “Hello Huan, would you like to learn some Python today?”
+
+name = "Huan"
+print(f"Hello {name.title()}! Would you like to learn some Python today?")
