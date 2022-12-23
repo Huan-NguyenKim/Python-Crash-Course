a0523342b657462d9f5d6b8536cca8ff24b2d184
Huan Nguyen <ngkimhuan@hotmail.com>
2022-12-22 22:23:16 -0800

    adding simple-messages.py to chapter2

---
+++ chapter-2/simple-messages.py
@@ -0,0 +1,8 @@
+#2-2. Simple Messages: Assign a message to a variable, and print that message.
+#Then change the value of the variable to a new message, and print the new message.
+
+message1 = "Hello everyone!"
+print(message1)
+
+message2 = "My name is Huan!"
+print(message2)
