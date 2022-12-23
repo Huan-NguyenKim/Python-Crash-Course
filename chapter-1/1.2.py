a8121580d73f0648263e817280380e40d64efb5c
Huan Nguyen <ngkimhuan@hotmail.com>
2022-12-22 21:18:53 -0800

    adding some comments to 1.2.py

--- chapter-1/1.2.py
+++ chapter-1/1.2.py
@@ -1,3 +1,8 @@
+#1-2. Hello World Typos: Open the hello_world.py file you just created. Make a
+# that generates an error? Can you make sense of the error message? Can you
+# make a typ#
+
 print("Hello World")
+
 #Can't have a typo because it will cause an error.
 # Instead, use # to write a comment. 
