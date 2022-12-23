600e6b879aba02ae23efca5bc3c10254e32b68e8
Huan Nguyen <ngkimhuan@hotmail.com>
2022-12-23 03:08:01 -0800

    updating famous-quote2.py

--- chapter-2/famous-quote2.py
+++ chapter-2/famous-quote2.py
@@ -2,3 +2,8 @@
 # famous person’s name using a variable called famous_person. Then compose
 # your message and represent it with a new variable called message. 
 # Print yourmessage.
+
+famous_person = "president john f. kennedy"
+famous_quote = 'once said, "Ask not what your country can do for you. Ask what you can do for your country!"'
+
+print(famous_person.title(), famous_quote)
