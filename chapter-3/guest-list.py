a713d8b0c6ab36300c01d22bbbaea74998182234
Huan Nguyen <ngkimhuan@hotmail.com>
2022-12-24 03:33:17 -0800

    updating gues-list.y

--- chapter-3/guest-list.py
+++ chapter-3/guest-list.py
@@ -3,4 +3,15 @@
 # invite to dinner. Then use your list to print a message to each person, inviting
 # them to dinner.
 
+#creating a list called "friends"
 friends = ['huy', 'hoang', 'liem']
+
+#creating messages
+message1 = f"Would you like to join me for dinner, {friends[0].title()}?"
+message2 = f"Would you like to join me for dinner, {friends[1].title()}?"
+message3 = f"Would you like to join me for dinner, {friends[2].title()}?"
+
+#Printing the output
+print(message1)
+print(message2)
+print(message3)
