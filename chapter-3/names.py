1348735d4529273dc48b23d44fe91c3eb48b3a4a
Huan Nguyen <ngkimhuan@hotmail.com>
2022-12-24 02:39:03 -0800

    creating greetings.py to chapter 3

--- chapter-3/names.py
+++ chapter-3/names.py
@@ -1,4 +1,10 @@
 # 3-1. Names: Store the names of a few of your friends in a list called names. Print
 # each person’s name by accessing each element in the list, one at a time.
 
-s
+names = ['Huy', 'Hoang', 'Liem', 'Minh', 'Linh']
+
+print(names[0].title()) #print first person's name
+print(names[1].title()) #print 2nd person's name
+print(names[2].title()) #print 3rd person's name
+print(names[3].title()) #print 4th person's name
+print(names[4].title()) #print 5th person's name
