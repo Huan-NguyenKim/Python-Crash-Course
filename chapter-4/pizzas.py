5d29f5b6474f4c9dc412e1c7c29e2521970ba269
Huan Nguyen <ngkimhuan@hotmail.com>
2022-12-27 02:32:08 -0800

    updating pizzas.py 1st time

--- chapter-4/pizzas.py
+++ chapter-4/pizzas.py
@@ -7,3 +7,13 @@
 # how much you like pizza. The output should consist of three or more lines
 # about the kinds of pizza you like and then an additional sentence, such as
 # I really love pizza!
+
+
+#Answer
+
+# Think of at least three kinds of your favorite pizza. Store these
+# pizza names in a list, and then use a for loop to print the name of each pizza
+
+pizzas = ['pizza1','pizza2','pizza3']
+for pizza in pizzas:
+    print(pizza)
