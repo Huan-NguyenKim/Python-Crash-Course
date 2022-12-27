e014370d91df9600ad66b12530e24606254657a9
Huan Nguyen <ngkimhuan@hotmail.com>
2022-12-27 02:37:09 -0800

    final update of pizzas.py

--- chapter-4/pizzas.py
+++ chapter-4/pizzas.py
@@ -23,3 +23,10 @@ for pizza in pizzas:
 #   instead of printing just the name of the pizza. For each pizza you should
 # have one line of output containing a simple statement like I like pepperoni pizza.
     print(f"\nI really like {pizza.title()}")
+
+
+#   •	 Add a line at the end of your program, outside the for loop, that states
+# how much you like pizza. The output should consist of three or more lines
+# about the kinds of pizza you like and then an additional sentence, such as
+# I really love pizza!
+print("\nI love all kinds of pizzas")
