252692b03a27e7433c2ec20c503ef5285b3fc3ab
Huan Nguyen <ngkimhuan@hotmail.com>
2022-12-28 03:30:37 -0800

    updating threes.py

--- chapter-4/cubes.py
+++ chapter-4/cubes.py
@@ -2,3 +2,4 @@
 # the cube of 2 is written as 2**3 in Python. Make a list of the first 10 cubes (that
 # is, the cube of each integer from 1 through 10), and use a for loop to print out
 # the value of each cube.
+
--- chapter-4/threes.py
+++ chapter-4/threes.py
@@ -1,5 +1,5 @@
 # 4-7. Threes: Make a list of the multiples of 3 from 3 to 30. Use a for loop to
 # print the numbers in your list.
 
-for multiple_number in range(3,31,1*3):
+for multiple_number in range(3,31,3):  #The program starts with 3. Then, it adds 3 until the value reaches 31.
     print(multiple_number)
