6fed15c4797dcaf26a355a57520a792e2c49a28d
Huan Nguyen <ngkimhuan@hotmail.com>
2022-12-29 03:34:54 -0800

    final update

--- chapter-4/buffet.py
+++ chapter-4/buffet.py
@@ -20,16 +20,30 @@ print("The buffet has 4 special foods. They are:")
 for food in foods:
     print(food.title())
 
+
+'''
+
 # •	 Try to modify one of the items, and make sure that Python rejects the change.
 foods[1] = ("rice")
 print("\nThe new list of foods are:")
 for food in foods:
     print(food.title())
-'''
+
 Output error: 
 
 Traceback (most recent call last):
   File "i:\Practice Computer Science\Python-Crash-Course\chapter-4\buffet.py", line 24, in <module>
     foods[1] = ("rice")
 TypeError: 'tuple' object does not support item assignment
+
 '''
+
+
+# •	 The restaurant changes its menu, replacing two of the items with different
+# foods. Add a line that rewrites the tuple, and then use a for loop to print
+# each of the items on the revised menu.
+
+foods = ("lobster", "noodles","salad", "bbq")
+print("\nThe new menu is:")
+for food in foods:
+    print(food.title())
