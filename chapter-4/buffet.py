d10ae14304e971c36ab75d55bbf614696f60b5ca
Huan Nguyen <ngkimhuan@hotmail.com>
2022-12-29 03:31:56 -0800

    2nd update

--- chapter-4/buffet.py
+++ chapter-4/buffet.py
@@ -13,9 +13,23 @@ each of the items on the revised menu.
 # Buffet: A buffet-style restaurant offers only five basic foods. Think of five
 # simple foods, and store them in a tuple.
 
-foods = ('lobster', 'sushi','salad', 'soup')
+foods = ("lobster", "sushi","salad", "soup")
 
 #  Use a for loop to print each food the restaurant offers.
 print("The buffet has 4 special foods. They are:")
 for food in foods:
     print(food.title())
+
+# •	 Try to modify one of the items, and make sure that Python rejects the change.
+foods[1] = ("rice")
+print("\nThe new list of foods are:")
+for food in foods:
+    print(food.title())
+'''
+Output error: 
+
+Traceback (most recent call last):
+  File "i:\Practice Computer Science\Python-Crash-Course\chapter-4\buffet.py", line 24, in <module>
+    foods[1] = ("rice")
+TypeError: 'tuple' object does not support item assignment
+'''
