af25bf54d3c5eb83007c43c0f1d019899a75827a
Huan Nguyen <ngkimhuan@hotmail.com>
2022-12-29 02:19:58 -0800

    2nd update of slices.py

--- chapter-4/slices.py
+++ chapter-4/slices.py
@@ -17,13 +17,13 @@ countries = ['usa','france','vietnam','china','canada','uk']
 
 #   •	 Print the message The first three items in the list are:. Then use a slice to
 # print the first three items from that program’s list.
-print("The first three items in the list are:")
+print("The first three countries in the list are:")
 for country1 in countries[:3]:
     print(country1.title())
 
 
 #   •	 Print the message Three items from the middle of the list are:. Use a slice to
 # print three items from the middle of the list.
-print()
-for country2 in countries[4:6]:
-    print(country2.print())
+print("\nThe next 3 countries are:")
+for country2 in countries[3:6]:
+    print(country2.title())
