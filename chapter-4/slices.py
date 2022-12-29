977958a9286130986a05ad36e97b62da78ecd512
Huan Nguyen <ngkimhuan@hotmail.com>
2022-12-29 02:18:33 -0800

    the 1st update of slices.py

--- chapter-4/slices.py
+++ chapter-4/slices.py
@@ -6,3 +6,24 @@
 # print three items from the middle of the list.
 #   •	 Print the message The last three items in the list are:. Use a slice to print the
 # last three items in the list.
+
+
+#Answer
+# Slices: Using one of the programs you wrote in this chapter, add several
+# lines to the end of the program that do the following:
+
+countries = ['usa','france','vietnam','china','canada','uk']
+
+
+#   •	 Print the message The first three items in the list are:. Then use a slice to
+# print the first three items from that program’s list.
+print("The first three items in the list are:")
+for country1 in countries[:3]:
+    print(country1.title())
+
+
+#   •	 Print the message Three items from the middle of the list are:. Use a slice to
+# print three items from the middle of the list.
+print()
+for country2 in countries[4:6]:
+    print(country2.print())
