4c8fec03cc88d9b29a16d4a470b556b35a7582f2
Huan Nguyen <ngkimhuan@hotmail.com>
2022-12-29 02:59:29 -0800

    1st update

--- chapter-4/more-loops.py
+++ chapter-4/more-loops.py
@@ -4,3 +4,28 @@ for loops when printing to save space. Choose a version of foods.py, and
 write two for loops to print each list of foods.
 
 """
+
+
+
+'''
+Original program from the book (page 63)
+
+my_foods = ['pizza', 'falafel', 'carrot cake']
+friend_foods = my_foods[:]
+print("My favorite foods are:")
+print(my_foods)
+print("\nMy friend's favorite foods are:")
+print(friend_foods)
+
+'''
+
+my_foods = ['pizza', 'falafel', 'carrot cake']
+print("My favorite foods are:")
+for my_food in my_foods[:]:
+    print(my_food.title())
+
+
+
+
+friend_foods = my_foods[:]
+
