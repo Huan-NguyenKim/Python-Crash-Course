aba091bdb395ec9fdaaf374a837ed6cb62d6cd27
Huan Nguyen <ngkimhuan@hotmail.com>
2022-12-31 04:58:20 -0800

    testing sorting chapter 1

--- chapter-1/1.2.py
+++ chapter-1/1.2.py
@@ -1,6 +1,6 @@
 #1-2. Hello World Typos: Open the hello_world.py file you just created. Make a
 # that generates an error? Can you make sense of the error message? Can you
-# make a typ#
+# make a type
 
 print("Hello World")
 
