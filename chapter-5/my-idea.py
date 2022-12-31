59485a6f625d9f24d3c1b5c1f51b2931fcd3bac2
Huan Nguyen <ngkimhuan@hotmail.com>
2022-12-31 04:38:51 -0800

    test sorting

--- chapter-5/my-idea.py
+++ chapter-5/my-idea.py
@@ -5,5 +5,5 @@ real-world situations are modeled in programs, you might be thinking of some
 problems you could solve with your own programs. Record any new ideas you
 have about problems you might want to solve as your programming skills continue to improve. 
 Consider games you might want to write, data sets you might
-want to explore, and web applications you’d like to create.
+want to explore, and web applications you’d like to create..
 '''
