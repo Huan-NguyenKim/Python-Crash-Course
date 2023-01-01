b14a0e33d1b096e647d7219551aa3f71b308f3cc
Huan Nguyen <ngkimhuan@hotmail.com>
2023-01-01 02:35:40 -0800

    final update of alien-color-3.py

--- chapter-5/5.5-alien-color-3.py
+++ chapter-5/5.5-alien-color-3.py
@@ -10,7 +10,7 @@ for the appropriate color alien.
 
 # 1/ If the alien is green, print a message that the player earned 5 points
 alien_color1 = "green"
-print("Is the alien's color green?")
+print("Is the alien's color green? (1st program)")
 
 if alien_color1 == "green":
     print("Yes, the alien's color is green. You have just earned 5 points (Output from IF).")
@@ -24,7 +24,7 @@ print("\n")
 
 # 2/ If the alien is yellow, print a message that the player earned 10 points.
 alien_color2 = "yellow"
-print("Is the alien's color green?")
+print("Is the alien's color green? (2nd program)")
 
 if alien_color2 == "green":
     print("Yes, the alien's color is green. You have just earned 5 points.")
@@ -32,3 +32,18 @@ elif alien_color2 == "yellow":
     print("No, the alien's color is yellow. You have earned 10 points (Output from ELIF).")
 else:
     print("No. The alien's color is red. You have earned 15 points.")
+
+print("\n")
+
+
+# 3/ If the alien is red, print a message that the player earned 15 points.
+alien_color3 = "red"
+print("Is the alien's color green? (3rd program)")
+
+if alien_color3 == "green":
+    print("Yes, the alien's color is green. You have just earned 5 points.")
+elif alien_color3 == "yellow":
+    print("No, the alien's color is yellow. You have earned 10 points.")
+else:
+    print("No, the alien's color is red. You have earned 15 points. (Output from ELSE)")
+
