686152a071c24822032be603f999ebd2e837ba44
Huan Nguyen <ngkimhuan@hotmail.com>
2023-01-01 02:22:37 -0800

    final update of alien-color-2.py

--- chapter-5/5.4-alien-color-2.py
+++ chapter-5/5.4-alien-color-2.py
@@ -16,7 +16,20 @@ runs the else block.
 # one version of this program that runs the if block (means printing the output from IF)
 
 alien_color1 = "red" 
+print("Is the alien's color red?")
 if alien_color1 == "red":
-    print("The alien's color is red. You have just earned 5 points.")
+    print("Yes, the alien's color is red. You have just earned 5 points (output from IF).")
 else:
     print("So sorry")
+
+print("\n")
+
+
+# If the alien’s color isn’t green, print a statement that the player just earned 10 points.
+# The program that runs the else block (means printing output from ELSE)
+alien_color2 = "green"
+print("Is the alien's color red?")
+if alien_color2 == "red":
+    print("Yes")
+else:
+    print("NO! The alien's color is green. You have earned 10 points (output from ELSE)")
