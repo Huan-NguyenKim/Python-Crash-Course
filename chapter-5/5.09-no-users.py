3bc8b46e12f87d7b5c8d3b69d19f10e2de74bcfd
Huan Nguyen <ngkimhuan@hotmail.com>
2023-01-01 04:08:05 -0800

    1st update of no-users.py

--- chapter-5/5.09-no-users.py
+++ chapter-5/5.09-no-users.py
@@ -6,3 +6,19 @@ not empty.
 •	 Remove all of the usernames from your list, and make sure the correct
 message is printed.
 '''
+
+#Creating an empty list
+usernames = []
+
+
+# •	 If the username is 'admin', print a special greeting, such as Hello admin,
+# would you like to see a status report?
+# •	 Otherwise, print a generic greeting, such as Hello Jaden, thank you for
+# logging in again.
+if 
+
+for username in usernames:   #this will be used to return the names.
+    if username == "admin":  #checking whether or not "ADMIN" is in the list. Then, return a special output
+        print(f"Hello {username.title()}! Would you like to see a report?")
+    else:
+        print(f"Hello {username.title()}! Thank you for logging in!")
