87d21a7cb50608a2cf73c4d032d9bd307b9c3c99
Huan Nguyen <ngkimhuan@hotmail.com>
2023-01-01 04:05:22 -0800

    final update of hello-admin.py

--- chapter-5/5.08-hello-admin.py
+++ chapter-5/5.08-hello-admin.py
@@ -16,5 +16,20 @@ logging in again.
 # each user:
 
 usernames = ["huy","hoang","liem","toan","admin"]
-for username in usernames:
-    print("Hello",username.title())
+
+
+
+# •	 If the username is 'admin', print a special greeting, such as Hello admin,
+# would you like to see a status report?
+# •	 Otherwise, print a generic greeting, such as Hello Jaden, thank you for
+# logging in again.
+
+for username in usernames:   #this will be used to return the names.
+    if username == "admin":  #checking whether or not "ADMIN" is in the list. Then, return a special output
+        print(f"Hello {username.title()}! Would you like to see a report?")
+    else:
+        print(f"Hello {username.title()}! Thank you for logging in!")
+    
+
+
+   
