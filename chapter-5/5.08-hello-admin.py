a1f1aa9fd822465bf88e97cb1e90bd6ceac88b2f
Huan Nguyen <ngkimhuan@hotmail.com>
2023-01-01 03:40:44 -0800

    1st update of hello-admin.py

--- chapter-5/5.08-hello-admin.py
+++ chapter-5/5.08-hello-admin.py
@@ -9,3 +9,12 @@ would you like to see a status report?
 •	 Otherwise, print a generic greeting, such as Hello Jaden, thank you for
 logging in again.
 '''
+
+# Hello Admin: Make a list of five or more usernames, including the name
+#'admin'. Imagine you are writing code that will print a greeting to each user
+# after they log in to a website. Loop through the list, and print a greeting to
+# each user:
+
+usernames = ["huy","hoang","liem","toan","admin"]
+for username in usernames:
+    print("Hello",username.title())
