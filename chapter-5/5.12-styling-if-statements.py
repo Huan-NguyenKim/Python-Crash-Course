3765cfd0693b4ea624f4e4c341f902f7292a855e
Huan Nguyen <ngkimhuan@hotmail.com>
2023-01-03 00:25:17 -0800

    1st update of styling-if-statements.py in chapter 5

--- chapter-5/5.12-styling-if-statements.py
+++ chapter-5/5.12-styling-if-statements.py
@@ -2,3 +2,26 @@
 5-12. Styling if statements: Review the programs you wrote in this chapter, and
 make sure you styled your conditional tests appropriately
 '''
+
+# not a good style (still executable but not easily readable)
+print('Can i vote?')
+number=18
+if number<21:
+    print("Sorry! You cann't vote.")
+else:
+    print('Yes, you can vote') 
+
+print('\n')
+
+# a good style ( executable and easily readable by adding some spaces)
+
+print("What city is in California?")
+
+california = ['san francisco','los angeles']
+cities = ['san francisco','new york']
+
+for city in cities:
+    if city in california:
+        print(f'{city.title()} is in California')
+    else:
+        print(f'Sorry! {city.title()} is not in California.')
