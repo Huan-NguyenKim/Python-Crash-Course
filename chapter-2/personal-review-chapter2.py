392d5a6a9c2a675a62a311a72ccdcdbd785b2d5a
Huan Nguyen <ngkimhuan@hotmail.com>
2023-01-05 02:29:40 -0800

    2nd update

--- chapter-2/personal-review-chapter2.py
+++ chapter-2/personal-review-chapter2.py
@@ -3,4 +3,29 @@ The purpose for this extra exercise is to review all the materials in this chapt
 
 '''
 
-s
+
+'''
+1/ We add a variable named message. Every variable is connected to a
+value, which is the information associated with that variable.Then, print that variable
+
+'''
+city = "San Francisco is a city in California"
+print(city)
+
+
+'''2/ Naming and Using Variables
+•	 Variable names can contain only letters, numbers, and underscores.
+But you can call a variable message_1 but not 1_message.
+
+•	 Spaces are not allowed in variable names, but underscores can be used
+to separate words in variable names. For example, greeting_message
+works, but greeting message doesn't.
+
+•	 Avoid using Python keywords and function names as variable names. For example, keyword (print)
+
+•	 Variable names should be short but descriptive. For example, name is
+better than n, student_name is better than s_n. 
+
+•	 Be careful when using the lowercase letter l and the uppercas
+
+'''
