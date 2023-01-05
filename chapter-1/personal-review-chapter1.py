8e09c23c090c7dcdc51455ecc132a9cc2e9c526d
Huan Nguyen <ngkimhuan@hotmail.com>
2023-01-05 00:09:28 -0800

    final update of personal-review-chapter1.py

--- chapter-1/personal-review-chapter1.py
+++ chapter-1/personal-review-chapter1.py
@@ -8,7 +8,17 @@ The purpose for this extra exercise is to review all the materials in this chapt
 
 '''
 Some useful commands:
-    + git (add .) to add all new files
-    + git (commit -m"") to commit the added files to the directory.
-    + git (push origin main) to push all the new files to Github.
+    + "git add ." to add all new files
+    + "git commit -m"" " to commit the added files to the directory.
+    + "git push origin main" to push all the new files to Github.
+'''
+
+
+'''
+Some other good commands (not used regularly as the commands above)
+    + "cd name of folder + enter" to change to anothe directory.
+    + "ls" to show all contents
+    + "mkdir name of new folder" to make a new folder/directory
+    + "rm folder name" to delete a folder
+    + " cat > name of file" to create a file in a folder.
 '''
