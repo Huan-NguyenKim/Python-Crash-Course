df4bcfb5dfada01d2909c37471c4c5451499ccb0
Huan Nguyen <ngkimhuan@hotmail.com>
2023-01-05 03:31:27 -0800

    including output for 3.1.py

--- chapter-3/3.01-names.py
+++ chapter-3/3.01-names.py
@@ -3,8 +3,19 @@
 
 names = ['Huy', 'Hoang', 'Liem', 'Minh', 'Linh']
 
-print(names[0].title()) #print first person's name
-print(names[1].title()) #print 2nd person's name
-print(names[2].title()) #print 3rd person's name
-print(names[3].title()) #print 4th person's name
-print(names[4].title()) #print 5th person's name
+print('The 1st friend is ' , names[0].title()) #print first person's name
+print('The 2nd friend is ' , names[1].title()) #print 2nd person's name
+print('The 3rd friend is ' , names[2].title()) #print 3rd person's name
+print('The 4th friend is ' , names[3].title()) #print 4th person's name
+print('The 5th friend is ' , names[4].title()) #print 5th person's name
+
+
+'''The output:
+
+The 1st friend is  Huy
+The 2nd friend is  Hoang
+The 3rd friend is  Liem
+The 4th friend is  Minh
+The 5th friend is  Linh
+
+'''
