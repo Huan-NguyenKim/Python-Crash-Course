70af949fde20f493a8ee55e412948f14effb6f93
Huan Nguyen <ngkimhuan@hotmail.com>
2023-01-05 03:08:23 -0800

    5th update

--- chapter-2/personal-review-chapter2.py
+++ chapter-2/personal-review-chapter2.py
@@ -102,3 +102,53 @@ name1 = ' huan '
 print('7a/ Using rstrip() method', name1.rstrip())
 print('7b/ Using lstrip() method', name1.lstrip())
 print('7c/ Using rstrip() method', name1.strip())
+
+
+
+print('\n')
+
+'''8/ Avoiding Syntax Errors with Strings.
+
+If you want to display a ' ', you should use " " in the print()
+'''
+
+print('8a/ This message will not show single quotes '' because i use single quotes for print()')
+print("8b/ This message will show single quotes ' ' because i use double quotes for print()")
+
+
+
+print('\n')
+
+
+'''9/ Integers
+
+You can add (+), subtract (-), multiply (*), and divide (/) integers in Python. 
+'''
+
+add_integer = 1 + 2
+print('9a/ The result after adding integers (1 + 2) is' , add_integer)
+
+subtract_integer = 5 - 1
+print('9b/ The result after subtracting integers ( 5 - 1) is' , subtract_integer)
+
+multiply_integer = 4 * 3
+print('9c/ The result after multiplying integers (4 * 3) is ' , multiply_integer)
+
+divide__integer = 20 / 2
+print('9d/ The result after dividing integers (20 / 2) is ' , divide__integer)
+
+
+print('\'n')
+
+
+
+'''10/ Floats
+
+Python calls any number with a decimal point a float.
+'''
+
+float1 = 0.4 + 0.2
+print('The float number is (0.4 + 0.2)' , float1)
+
+
+print('\n')
