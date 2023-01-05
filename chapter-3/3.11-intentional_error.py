c607f78086f16e109214f790e94e09572bae3308
Huan Nguyen <ngkimhuan@hotmail.com>
2023-01-05 03:47:02 -0800

    including output for 3.11.py

--- chapter-3/3.11-intentional_error.py
+++ chapter-3/3.11-intentional_error.py
@@ -5,5 +5,19 @@
 
 list = ["hi", "hello","greetings","ni hao"]
 #print("Showing an index error", list[10])
+#This will show an index error because the list only has 4 items. It cann't return the 10th value as we want.
 
-print("Showing the correct list", list)
+print('a/ It shows an eror because the list only has 4 items but we use list[10]')
+
+
+
+# Correct way to use list
+print("b/ Showing the correct list", list)
+
+
+
+'''Output:
+
+a/ It shows an eror because the list only has 4 items but we use list[10]
+b/ Showing the correct list ['hi', 'hello', 'greetings', 'ni hao']
+'''
