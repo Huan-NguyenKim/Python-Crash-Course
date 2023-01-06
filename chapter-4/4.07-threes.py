5462829891143dc5a12941b059a65e8a72a28e76
Huan Nguyen <ngkimhuan@hotmail.com>
2023-01-06 02:26:43 -0800

    including output for 4.7

--- chapter-4/4.07-threes.py
+++ chapter-4/4.07-threes.py
@@ -2,4 +2,20 @@
 # print the numbers in your list..
 
 for multiple_number in range(3,31,3):  #The program starts with 3. Then, it adds 3 until the value reaches 31.
-    print(multiple_number)
+    print(multiple_number, 'is a multiple of 3')
+
+
+'''Output:
+
+3 is a multiple of 3
+6 is a multiple of 3
+9 is a multiple of 3
+12 is a multiple of 3
+15 is a multiple of 3
+18 is a multiple of 3
+21 is a multiple of 3
+24 is a multiple of 3
+27 is a multiple of 3
+30 is a multiple of 3
+
+'''
