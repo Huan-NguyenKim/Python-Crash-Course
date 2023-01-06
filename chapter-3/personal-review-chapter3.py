d3c284288ea7c2d70e42133e8fdf0a3d0586cc81
Huan Nguyen <ngkimhuan@hotmail.com>
2023-01-05 23:40:36 -0800

    fixing a typo from the last commit. I meant chapter 3

--- chapter-3/personal-review-chapter3.py
+++ chapter-3/personal-review-chapter3.py
@@ -238,7 +238,7 @@ For example, if your list has three items, and you ask for the fourth item, an e
     You learned to sort lists permanently and temporarily for
 display purposes. 
     You also learned how to find the length of a list and 
-how to avoid index errors when you’re working with lists.
+how to avoid index errors when you’re working with lists..
 '''
 
 
