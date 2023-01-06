ca906bd290ef138c6a1c3620337e252d97ae5355
Huan Nguyen <ngkimhuan@hotmail.com>
2023-01-05 23:36:18 -0800

    9th update

--- chapter-3/personal-review-chapter3.py
+++ chapter-3/personal-review-chapter3.py
@@ -221,3 +221,17 @@ len() method
 '''
 
 print('15/ The length of the list after using the len() method is', len(names))
+
+
+
+'''Summary
+
+    In this chapter you learned what lists are and how to work with the individual items in a list. 
+    You learned how to define a list and how to add and remove elements. 
+    You learned to sort lists permanently and temporarily for
+display purposes. 
+    You also learned how to find the length of a list and 
+how to avoid index errors when you’re working with lists.
+'''
+
+
