2930391b93f7f78dbd1840bc71321e06b7146bb0
Huan Nguyen <ngkimhuan@hotmail.com>
2023-01-05 22:26:08 -0800

    2nd update

--- chapter-3/personal-review-chapter3.py
+++ chapter-3/personal-review-chapter3.py
@@ -12,3 +12,28 @@ A list is a collection of items in a particular order
 
 list = ['a', 'b', 'c']
 print('1/ The elements of the list are: ', list)
+
+
+print('\n')
+
+
+'''2/ Accessing Elements in a List
+
+    You can access any element in a list by
+telling Python the position, or index
+'''
+
+print('2/ The 2nd item of the list is: ', list[1])
+
+
+print('\n')
+
+
+
+'''3/ Index Positions Start at 0, Not 1
+'''
+
+print('3/ Using list[0] to display the first item of the list, which is ', list[0])
+
+
+print('\n')
