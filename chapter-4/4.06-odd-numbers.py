e6874dfeec06f758dece5e153f44d0e308f30675
Huan Nguyen <ngkimhuan@hotmail.com>
2023-01-06 02:25:19 -0800

    including output for 4.6

--- chapter-4/4.06-odd-numbers.py
+++ chapter-4/4.06-odd-numbers.py
@@ -2,4 +2,20 @@
 # list of the odd numbers from 1 to 20. Use a for loop to print each number..
 
 for odd_number in range(1,21,2):  #This means the program starts with 1. Then, it add 2 every time until it passes 21.
-    print(odd_number)
+    print(odd_number, 'is an odd number')
+
+
+'''Output:
+
+1 is an odd number
+3 is an odd number
+5 is an odd number
+7 is an odd number
+9 is an odd number
+11 is an odd number
+13 is an odd number
+15 is an odd number
+17 is an odd number
+19 is an odd number
+
+'''
