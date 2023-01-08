80f8d15883aabb9f676f2a68be51549106258f9d
Huan Nguyen <ngkimhuan@hotmail.com>
2023-01-08 03:32:30 -0800

    2nd update

--- chapter-4/personal-review-chapter4.py
+++ chapter-4/personal-review-chapter4.py
@@ -3,4 +3,32 @@ The purpose for this extra exercise is to review all the materials in this chapt
 
 '''
 
-ss.
+
+'''1/ For Looping
+
+    Python prints the current value of list. Then,Python repeats the entire loop 
+until the last value in the list.
+    If there is no more values in the list, the program simply ends.
+
+'''
+
+cities = ['san francisco', 'los angeles', 'hayward']
+for city in cities:
+    print(f'1/ {city.title()} is a city in California')
+
+
+print('\n')
+
+
+
+'''2/ Doing Something After a for Loop
+
+    Any lines of code after the for loop that are not indented are executed
+once without repetition.
+'''
+
+cities2 = ['new york', 'houston', 'miami']
+for city2 in cities2:
+    print(f'2a/ {city2.title()} is city outside of California.')
+
+print('\n2b/ These cities are not in California (this statement is outside of the loop)')
